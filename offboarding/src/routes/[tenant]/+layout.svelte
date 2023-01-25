d6ab5e951bebe62c6fd8202765042a5ba49ddf31
Daniel Arroyo <daniel.arroyo@geopagos.com>
2023-01-25 09:13:06 -0300

    origin svelte

---
+++ offboarding/src/routes/[tenant]/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../../app.css';
+	import LayoutViumi from '../../components/LayoutViumi.svelte';
+	import NotFound from '../../components/NotFound.svelte';
+
+	/**@type {{ tenant:string }}*/
+	export let data;
+
+	/**@type {Record<string ,any>}*/
+	const layouts = {
+		viumi: LayoutViumi,
+		notFound: NotFound
+	};
+
+	export const LayoutPicked = data.tenant ? layouts[data.tenant] ?? NotFound : null;
+</script>
+
+<LayoutPicked><slot /></LayoutPicked>
---
+++ offboarding/src/routes/[tenant]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello DANI!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+		min-height: 100vh;
+	}
+</style>
