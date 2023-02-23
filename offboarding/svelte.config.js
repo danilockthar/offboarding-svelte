03500c71496de7bb599f44a15b61d6c40a17b4e7
Daniel Arroyo <daniel.arroyo@geopagos.com>
2023-02-23 17:44:06 -0300

    fix traefik prefix

--- offboarding/svelte.config.js
+++ offboarding/svelte.config.js
@@ -7,7 +7,7 @@ import sveltePreprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapterNode(),
