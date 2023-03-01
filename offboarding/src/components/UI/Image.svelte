2307e66dda78ebc8d1956b820eed79de626abca8
Daniel Arroyo <daniel.arroyo@geopagos.com>
2023-02-28 22:26:16 -0300

    added more screens & fixes

---
+++ offboarding/src/components/UI/Image.svelte
@@ -0,0 +1,12 @@
+<script>
+    /**@type {string}*/
+	export let tenant;
+
+    /**@type {string}*/
+	export let id;
+
+     /**@type {string}*/
+	export let src;
+</script>
+
+<img {id} src="/{tenant}/assets/{src}.svg" alt="imagen descriptiva">
---
+++ offboarding/src/services/unsubscribe.ts
@@ -0,0 +1,27 @@
+export const unsubscribe = () => {
+	const myHeaders = new Headers();
+	myHeaders.append(
+		'X-Authentication-Token',
+		'pt_450df86130c0c317b5a1793fd0d6a1306d1d83373fc7478035335a1b2e54a4c9'
+	);
+	myHeaders.append('X-Tenant', 'macro');
+	myHeaders.append('Content-Type', 'application/json');
+
+	const raw = JSON.stringify({
+		merchant_id: 388,
+		products: ['account']
+	});
+
+	const requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+
+	return fetch(
+		'https://api-macro.dev.geopagos.com/api/v1.0/Unsubscribe/',
+		/**@ts-ignore */
+		requestOptions
+	);
+};
